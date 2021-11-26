c88c1be55fec80bf59189951e3ac59271371ce87
kngurley <kngurley25@gmail.com>
2021-11-26 17:00:14 -0600

    add get request to home and notes html pages

--- server.js
+++ server.js
@@ -3,40 +3,50 @@ const fs = require("fs");
 const path = require("path");
 const notes = require("./db/db.json");
 
-//instantiate the server
+// instantiate the server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//middleware to parse incoming data
+// middleware to parse incoming data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("public"));
 
+// get home html page
+app.get("/", (req, res) => 
+    res.sendFile(path.join(__dirname, "/public/index.html"))
+);
 
-//server routes
+// get notes html page 
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+// get request for notes
 app.get("/api/notes", (req, res) => {
     res.json(notes);
-})
+});
 
 app.post("/api/notes", (req, res) => {
     
+    // destructure items in request body
     const { title, text } = req.body;
-    
     if (!title || !text) {
         res.json("Error in posting note");
-    }
-
+    };
+    // new object to save
     const newNote = {
         title, 
         text
-    }
-
+    };
     // obtain existing notes
     fs.readFile("./db/db.json", "utf8", (err, data) => {
         if (err) {
             console.error(err);
         }
 
+        // add new note
         const parsedNotes = JSON.parse(data);
         parsedNotes.push(newNote);
 
@@ -49,13 +59,13 @@ app.post("/api/notes", (req, res) => {
                 ? console.error(err) 
                 : console.log(`Note has been written to JSON file`)
         )
-    })
+    });
     const response = {
         status: "success",
         body: newNote
     };
     res.json(response);
-})
+});
 
 //tell server to listen to requests
 app.listen(PORT, () => {
