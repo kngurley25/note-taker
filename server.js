42dc42522578398e5cfe92865d322bb03c01700f
kngurley <kngurley25@gmail.com>
2021-11-26 10:22:36 -0600

    add post and middleware functions

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//middleware to parse incoming data
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 const { notes } = require("./db/db.json");
 
-
+//server routes
 app.get("/api/notes", (req, res) => {
     res.json(notes);
+})
+
+app.post("/api/notes", (req, res) => {
+    console.log(req.body);
+    res.json(req.body);
 })
 
 //tell server to listen to requests
