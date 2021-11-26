668868538980cece97c0aa24dcfe8660ba4fd71f
kngurley <kngurley25@gmail.com>
2021-11-26 16:12:27 -0600

    update post method to write to json file

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const notes = require("./db/db.json");
 
 //instantiate the server
 const app = express();
@@ -8,7 +11,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const { notes } = require("./db/db.json");
+
 
 //server routes
 app.get("/api/notes", (req, res) => {
@@ -16,8 +19,42 @@ app.get("/api/notes", (req, res) => {
 })
 
 app.post("/api/notes", (req, res) => {
-    console.log(req.body);
-    res.json(req.body);
+    
+    const { title, text } = req.body;
+    
+    if (!title || !text) {
+        res.json("Error in posting note");
+    }
+
+    const newNote = {
+        title, 
+        text
+    }
+
+    // obtain existing notes
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+        }
+
+        const parsedNotes = JSON.parse(data);
+        parsedNotes.push(newNote);
+
+        // write updated notes back to file
+        fs.writeFileSync(
+            path.join(__dirname, "./db/db.json"),
+            JSON.stringify(parsedNotes, null, 2),
+            (err) => 
+                err 
+                ? console.error(err) 
+                : console.log(`Note has been written to JSON file`)
+        )
+    })
+    const response = {
+        status: "success",
+        body: newNote
+    };
+    res.json(response);
 })
 
 //tell server to listen to requests
