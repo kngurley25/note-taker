740d1e0a49ab05fb5ec446a82f65b1b21d744535
kngurley <kngurley25@gmail.com>
2021-11-26 09:54:02 -0600

    add route for client to access API call

--- server.js
+++ server.js
@@ -2,11 +2,14 @@ const express = require("express");
 
 //instantiate the server
 const app = express();
-
 const PORT = process.env.PORT || 3001;
 
+const { notes } = require("./db/db.json");
 
 
+app.get("/api/notes", (req, res) => {
+    res.send("testing");
+})
 
 //tell server to listen to requests
 app.listen(PORT, () => {
